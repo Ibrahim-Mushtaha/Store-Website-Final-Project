6cbdaa49a14775f817ab6ea45b26a4be52b45c0f
ibrahim mushtaha <ibrahim.mushtaha2@gmail.com>
2021-12-08 20:20:28 +0200

    - update store home view

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -189,26 +189,6 @@
                                         </div>
                                    </form>
                                 @endif
-                                {{-- <form method="POST" action="{{URL("form/view/rating")}}" >
-                                    @csrf
-                                    <div class="rate">
-                                        <input  type="radio" id="star5" name="rate" value="5" />
-                                        <label for="star5" title="text">5 stars</label>
-                                        <input type="radio" id="star4" name="rate" value="4" />
-                                        <label for="star4" title="text">4 stars</label>
-                                        <input type="radio" id="star3" name="rate" value="3" />
-                                        <label for="star3" title="text">3 stars</label>
-                                        <input type="radio" id="star2" name="rate" value="2" />
-                                        <label for="star2" title="text">2 stars</label>
-                                        <input type="radio" id="star1" name="rate" value="1" checked/>
-                                        <label for="star1" title="text">1 star</label>
-                                        <input type="hidden" id="store_id" name="store_id" value="{{$store->id}}" />
-                                        <input type="hidden" name="userIP" value="43">
-                                    </div>
-                                    <div class="action">
-                                        <button class="add-to-cart btn btn-success" type="submit">Send Rating</button>
-                                    </div>
-                                </form> --}}
                             </div>
                         </div>
                         </div>
